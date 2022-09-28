92d9979418507aa34832c792eca165278970af3c
MFolgado <marcelofolgado@gmail.com>
2022-09-28 15:51:52 -0400

    first commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return this.coursesService.findOne(params.id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.coursesService.remove(id);
+  }
+}
---
+++ src/courses/entities/course.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Tag } from './tag.entity';
+
+@Entity('courses')
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @JoinTable()
+  @ManyToMany( () => Tag, (tag: Tag) => tag.courses, {
+    cascade: true
+  })
+  tags: Tag[];
+}
